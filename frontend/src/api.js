22421e3f32f6d54f0680113d802f1bd744f45d29
Robert Hoffmann <rob.hoffmann@posteo.de>
2024-06-20 14:34:36 +0000

    removed port from prod url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://bfarm-referenzdatenbank-explorer.onrender.com:8000";
+const API_BASE_URL = "http://bfarm-referenzdatenbank-explorer.onrender.com";
 const API_BASE_URL_dev = "http://127.0.0.1:5000";
 
 
